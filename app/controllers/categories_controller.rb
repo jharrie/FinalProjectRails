81697b3c745d01d6918ee54773d27b8d99778441
Jharrie <jharrie@gmail.com>
2020-11-13 20:57:17 -0600

    completed layout

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,6 @@
 class CategoriesController < InheritedResources::Base
 
-  private
-
     def category_params
       params.require(:category).permit(:name, :product_id, :service_id, :description)
     end
-
 end
