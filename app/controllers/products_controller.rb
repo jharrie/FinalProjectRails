ffbeca31e29c719300d032c8821adb8efbc43c07
Jharrie <jharrie@gmail.com>
2020-11-19 08:08:30 -0600

    Corrected layouts

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,13 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[show edit update destroy]
+  before_action :set_product, only: %i[show]
 
   # TODO: NEED TO ADD RANSACK AND PAGINATION.
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @search = Product.ransack(params[:q])
+    @products = @search.result.page(params[:page])
+    @search.build_condition
   end
 
   # GET /products/1
@@ -21,46 +23,6 @@ class ProductsController < ApplicationController
 
   # GET /products/1/edit
   def edit; end
-
-  # POST /products
-  # POST /products.json
-  def create
-    @product = Product.new(product_params)
-
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: "Product was successfully created." }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /products/1
-  # PATCH/PUT /products/1.json
-  def update
-    respond_to do |format|
-      if @product.update(product_params)
-        format.html { redirect_to @product, notice: "Product was successfully updated." }
-        format.json { render :show, status: :ok, location: @product }
-      else
-        format.html { render :edit }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /products/1
-  # DELETE /products/1.json
-  def destroy
-    @product.destroy
-    respond_to do |format|
-      format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
 
   private
 
