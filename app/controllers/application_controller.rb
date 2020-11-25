726501065be0e04ee578b695dcc213255b35e91f
Jharrie <jharrie@gmail.com>
2020-11-25 10:47:31 -0600

    Before Kyle assistance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,27 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :init_session
   helper_method :cart
-
-  private
 
   def init_session
     session[:shopping_cart] ||= []
     session[:cart_quantity] ||= []
+
+    @users = User.all
+    @provinces = Province.all
   end
 
   def cart
     # return a collection product objects based on the product_id in the cart
     Product.find(session[:shopping_cart])
+  end
+
+  def final_cart
+    session[:final_cart] = []
+    session[:shopping_cart].map do |final|
+      quantity_complete = session[:shopping_cart][final["id"]]
+      finish = final.map { |id, quantity| [Product.find(id.to_i), quantity_complete["quantity"]] }.to_h
+    end
   end
 
   def configure_permitted_parameters
--- app/views/carts/_cart.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! cart, :id, :total, :balance, :itemtotal, :customer_id, :product_id, :service_id, :pst, :gst, :hst, :created_at, :updated_at
-json.url cart_url(cart, format: :json)
